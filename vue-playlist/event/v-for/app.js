1cd38701b07f98343a8812039b078dfef968d833
EvenZZZ <ezhu0915@163.com>
2018-09-12 08:35:41 +0800

    v-if v-for Demo

---
+++ vue-playlist/event/v-for/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el:"#vue-app",
+	data:{
+		characters:["Mario","Luffy","Naruto"],
+		users:[
+			{name:"Henry",age:20},
+			{name:"Bucky",age:25},
+			{name:"Emily",age:30}
+		]
+	},
+	methods:{
+		
+	},
+	computed:{
+		
+	}
+});
