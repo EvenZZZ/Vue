bf23bb9b9e4ae7ab44a1b9a55c1144e7c846060f
EvenZZZ <ezhu0915@163.com>
2018-09-20 21:48:56 +0800

    start vue-cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<!-- 1模板: html结构 -->
+<!-- 有且只能有一个根元素 -->
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <users></users>
+  </div>
+</template>
+
+<!-- 2行为: 处理逻辑 -->
+<script>
+
+// 局部注册组件
+import Users from './components/Users'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title:"这是我的第一个Vue脚手架项目！"
+    }
+  },
+  components:{
+  	// "users":Users
+  	Users
+  }
+}
+</script>
+
+<!-- 3样式: 解决样式 -->
+<style>
+
+</style>
