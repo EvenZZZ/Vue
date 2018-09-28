dde09231f05c726e10a05f45479c0707b81daf26
EvenZZZ <ezhu0915@163.com>
2018-09-28 23:03:04 +0800

    add Contact component

--- pizza-app/src/main.js
+++ pizza-app/src/main.js
@@ -8,6 +8,16 @@ import About from './components/about/About'
 import Login from './components/Login'
 import Register from './components/Register'
 
+// 二级路由
+import Contact from './components/about/Contact'
+import Delivery from './components/about/Delivery'
+import History from './components/about/History'
+import OrderingGuide from './components/about/OrderingGuide'
+
+//三级路由
+import Phone from './components/about/contact/Phone'
+import PersonName from './components/about/contact/PersonName'
+
 Vue.use(VueRouter)  //使用路由
 
 //配置路由
@@ -15,7 +25,15 @@ const routes = [
 	{path:'/',name:"homelink",component:Home},
 	{path:'/menu',name:"menulink",component:Menu},
 	{path:'/admin',name:"adminlink",component:Admin},
-	{path:'/about',name:"aboutlink",component:About},
+	{path:'/about',name:"aboutlink",redirect:'/about/contact',component:About, children:[
+		{path:'/about/contact',name:'contactLink',redirect:'/personName',component:Contact,children:[
+			{path:'/phone',name:'phoneNumber',component:Phone},
+			{path:'/personName',name:'personName',component:PersonName},
+		]},
+		{path:'/history',name:'historyLink',component:History},
+		{path:'/delivery',name:'deliveryLink',component:Delivery},
+		{path:'/orderingGuide',name:'orderingGuideLink',component:OrderingGuide},
+	]},
 	{path:'/login',name:"loginlink",component:Login},
 	{path:'/register',name:"registerlink",component:Register},
 	{path:"*",redirect:'/'}
